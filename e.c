1d9d0fe5c3a91e5828c4caf3381865be38d0199f
David Reid <mackron@gmail.com>
2025-03-24 17:53:23 +1000

    Update c89thread amalgamation.

--- e.c
+++ e.c
@@ -4117,8 +4117,8 @@ static int e_pthread_mutex_timedlock(pthread_mutex_t* mutex, const struct timesp
     Fallback implementation for when pthread_mutex_timedlock() is not avaialble. This is just a
     naive loop which waits a bit of time before continuing.
     */
-    #if !defined(C89ATOMIC_SUPPRESS_FALLBACK_WARNING) && !defined(__APPLE__)
-        #warning pthread_mutex_timedlock() is unavailable. Falling back to a suboptimal implementation. Set _XOPEN_SOURCE to >= 600 to use the native implementation of pthread_mutex_timedlock(). Use C89ATOMIC_SUPPRESS_FALLBACK_WARNING to suppress this warning.
+    #if !defined(E_SUPPRESS_MUTEX_TIMEDLOCK_FALLBACK_WARNING) && !defined(__APPLE__)
+        #warning pthread_mutex_timedlock() is unavailable. Falling back to a suboptimal implementation. Set _XOPEN_SOURCE to >= 600 to use the native implementation of pthread_mutex_timedlock(). Use E_SUPPRESS_MUTEX_TIMEDLOCK_FALLBACK_WARNING to suppress this warning.
     #endif
 
     int result;
