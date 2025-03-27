20a92f591335fce823b345b7bf0456a232d32ab0
David Reid <mackron@gmail.com>
2025-03-27 12:46:57 +1000

    Amalgamate some file system docs.

--- e.h
+++ e.h
@@ -1204,11 +1204,73 @@ extern const e_fs_backend* E_FS_STDIO;  /* The default stdio backend. The handle
 
 
 /* BEG e_fs_zip.h */
+/*
+Zip file support.
+
+This only supports STORE and DEFLATE. It does not support DEFLATE64.
+
+To use this, you'll first need to a fs_stream containing a Zip archive file. You can get this
+easily from a fs object.
+
+    fs_file* pZipArchiveFile;
+    fs_file_open(pFS, "archive.zip", FS_READ, &pZipArchiveFile); // Assumes pFS was initialized earlier.
+
+    ...
+
+    fs* pZip;
+    fs_init(FS_ZIP, NULL, fs_file_get_stream(pZipArchiveFile), NULL, &pZip);
+
+    ...
+
+    fs_file* pFileInsideZip;
+    fs_file_open(pZip, "file.txt", FS_READ, &pFileInsideZip);
+
+    ... now just read from pFileInsideZip like any other file ...
+
+A Zip archive is its own file system and is therefore implemented as an fs backend. In order to
+actually use the backend, it needs to have access to the raw data of the entire Zip file. This is
+supplied via a fs_stream object. The fs_file object is a stream and can be used for this purpose.
+The code above just opens the Zip file from an earlier created fs object.
+
+Once you have the fs_stream object for the Zip file, you can initialize a fs object, telling it to
+use the Zip backend which you do by passing in FS_ZIP, which is declared in this file. If all goes
+well, you'll get a pointer to a new fs object representing the Zip archive and you can use it to
+open files from within it just like any other file.
+
+You can pass in NULL for the backend config in fs_init().
+*/
 extern const e_fs_backend* E_FS_ZIP;
 /* END e_fs_zip.h */
 
 
 /* BEG e_fs_subfs.h */
+/*
+This backend can be used to create a filesystem where the root directory of the new fs object is
+a subdirectory of another fs object. Trying to access anything outside of this root directory
+will result in an error.
+
+Everything is done in terms of the owner FS object, meaning the owner must be kept alive for the
+life of the subfs object. It also means it will inherit the owner's registered archive types.
+
+The way this works is very simple. Whenever you try to open a file, the path is checked to see if
+it's attempting to access anything outside of the root directory. If it is, an error is returned.
+Otherwise, the root directory is prepended to the path and the operation is passed on to the owner
+FS object.
+
+To use this backend, you need to create a fs_subfs_config object and fill in the pOwnerFS and
+pRootDir fields. Then pass this object into `fs_init()`:
+
+    fs_subfs_config subfsConfig;
+    subfsConfig.pOwnerFS = pOwnerFS;
+    subfsConfig.pRootDir = "subdir";
+
+    fs_config fsConfig = fs_config_init(FS_SUBFS, &subfsConfig, NULL);
+
+    fs* pSubFS;
+    fs_init(&fsConfig, &pSubFS);
+
+You must ensure you destroy your subfs object before destroying the owner FS object.
+*/
 typedef struct e_subfs_config
 {
     e_fs* pOwnerFS;
